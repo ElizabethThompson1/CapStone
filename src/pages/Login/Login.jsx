3d7140f3c5c275f32b8a7dc87eed6b1473209663
ElizabethThompson1 <usawsoccer11@yahoo.com>
2022-01-12 10:15:25 -0600

    created a showpassword button and applied css to it

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,17 +2,23 @@ import React, {useState} from 'react';
 import "./Login.css";
 import { Link } from 'react-router-dom';
 
+
 const Login = () => {
     const[email,setEmail] = useState("");
     const[password,setPassword] = useState("");  
+    const[showpass,setShowpass] =useState(false);
+
     return (
         <div className='login'>
             <h3 className='login-header'>Friend U</h3>
             <h6 className='login-subheader'>Login</h6>
             <div className='login-data'>
                 <form className='login-dataform'>
-                    <input className="login-input" value={email} onChange={(e) => setEmail(e.target.email)} placeholder="Enter Email"></input>
-                    <input className="login-input" value={password} onChange={(e)=> setPassword(e.target.password)} placeholder="Enter Password"></input>
+                    <input className="login-input" value={email} onChange={(e) => setEmail(e.target.email)} type="email" placeholder="Enter Email"></input>
+                    <input className="login-input" value={password} onChange={(e)=> setPassword(e.target.password)} type={showpass ? "type": "password"} placeholder="Enter Password"></input>
+                    <div className='showpass-button'>
+                    <small className='showpass-login' onClick={()=>setShowpass(!showpass)}>show</small>
+                    </div>
                     <button className='login-button' type="submit">Login</button>
                     <small className='create-account'>Do not have account<Link className="login-register" to="/register">Here</Link></small>
                 </form>
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,11 +1,13 @@
 import React,{useState} from 'react'
-import "./Register.css"
+import "./Register.css";
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     const[email,setEmail] = useState("");
     const[password,setPassword] = useState("");  
     const[username,setUserName] = useState("");  
     const[fullname,setFullName] = useState("");  
+    const[showpass,setShowpass] = useState(false);
 
     return (
         <div className='register'>
@@ -17,7 +19,11 @@ const Register = () => {
                     <input className="register-input" value={username} onChange={(e)=> setUserName(e.target.username)} placeholder="Enter Username"></input>
                     <input className="register-input" value={email} onChange={(e) => setEmail(e.target.email)} placeholder="Enter Email"></input>
                     <input className="register-input" value={password} onChange={(e)=> setPassword(e.target.password)} placeholder="Enter Password"></input>
+                    <div className='showpass-button-registration'>
+                    <small className='showpass-registration' onClick={()=>setShowpass(!showpass)}>show</small>
+                    </div>
                     <button className='register-button' type="submit">Login</button>
+                    <small className='goto-account'>Have an Account<Link className="register-login" to="/">Login</Link></small>
                 </form>
             </div>
         </div>
