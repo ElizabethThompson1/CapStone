6194d517ddaa84c8cef93f556e42be0f62061f93
ElizabethThompson1 <usawsoccer11@yahoo.com>
2022-01-14 12:24:36 -0600

    made parts for the alerts and about the user that is signed in on

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,12 @@
+import React from 'react';
+import "../styles/ProfileAbout.css"
 
-
-
+import moment from 'moment'
 
 
 const About = ({userData, dispatch, auth, profile,  id} ) =>{
 
-    
+
 
 return (
         <div className="profileabout">
@@ -16,12 +17,28 @@ return (
                         
                     </div>
                     <div  className="profileabout-contentcenter">
-                            <p className="profileabout-contentcenter-story">{user.bio}</p>
+                            <p className="profileabout-contentcenter-story">{user.story}</p>
                     </div>
                     <div className="profileabout-contentbottom">
                         <div className="profileabout-contentbottominfo">
+                        <h6 className="profileabout-contentbottominfo-head"> Joined </h6>
+                        <p className='profileabout-contentbottominfo-body'> {moment(user.createdAt).format('YYYY-MM-DD')}</p>
+                        </div>
+                        <div className="profileabout-contentbottominfo">
+                        <h6 className="profileabout-contentbottominfo-head"> Gender </h6>
+                        <p className='profileabout-contentbottominfo-body'> {user.gender}</p>
+                        </div>
+                        <div className="profileabout-contentbottominfo">
+                        <h6 className="profileabout-contentbottominfo-head"> Phone </h6>
+                        <p className='profileabout-contentbottominfo-body'> {user.phone}</p>
+                        </div>
+                        <div className="profileabout-contentbottominfo">
                         <h6 className="profileabout-contentbottominfo-head"> Email </h6>
                         <p className='profileabout-contentbottominfo-body'> {user.email}</p>
+                        </div>
+                        <div className="profileabout-contentbottominfo">
+                        <h6 className="profileabout-contentbottominfo-head"> Website </h6>
+                        <a href={`${user.website}`}  target="_blank" rel="noopener noreferrer" className='profileabout-contentbottominfo-body'> {user.website} </a>
                         </div>
                     </div>
                 </div>
